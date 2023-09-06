48aee689b3e3e6cb9f21685742dc2c476493ded1
Jeff Nueva <141053012+nuevaJ@users.noreply.github.com>
2023-09-06 18:59:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const game = () => {
 
-    const playerSelection = prompt("Bato, Papel, o Gunting", "");
-
     const getComputerChoice = () => {
-        let computerChoices = ['bato', 'papel', 'gunting'];
+        const computerChoices = ['bato', 'papel', 'gunting'];
     
         return computerChoices[Math.floor(Math.random() * computerChoices.length)]
     }
@@ -13,22 +11,18 @@ const game = () => {
         playerSelection = playerSelection.toLowerCase();
     
         if (playerSelection === computerSelection){
-            return `It's a tie! ${playerSelection} same with ${computerSelection}`;
+            return `It's a tie! ${playerSelection} same with ${computerSelection}!`;
         } else if 
         (playerSelection === "bato" && computerSelection === "gunting" ||
         playerSelection === "gunting" && computerSelection === "papel" ||
         playerSelection === "papel" && computerSelection === "bato"){
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
         }else{
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            return `You lose! ${computerSelection} beats ${playerSelection}!`;
         }
-
     }
-        
+        playerSelection = prompt("Bato, Papel, o Gunting", "");
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
-    
-        playRound();
 }
-
 game();
